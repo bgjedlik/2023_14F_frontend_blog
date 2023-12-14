c8f08e2f05de5754ed8fd98d5e47c57f1344a732
bgjedlik <bolya.gabor@jedlik.eu>
2023-12-14 11:31:05 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import AppHeader from './components/AppHeader.vue';
+import AppFooter from './components/AppFooter.vue';
+import AppNav from './components/AppNav.vue';
+</script>
+
+<template>
+  <AppHeader/>
+  <AppNav/>
+  <RouterView />
+  <AppFooter/>
+</template>
+
+<style scoped>
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="container">
+    OK
+
+  </div>
+</template>
